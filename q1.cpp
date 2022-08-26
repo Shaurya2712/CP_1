c56f6065362509d97cff826dab9d35c3ab53c42f
samridhi14 <83155658+Shaurya2712@users.noreply.github.com>
2022-08-26 15:32:00 +0530

    q1

--- q1.cpp
+++ q1.cpp
@@ -1,6 +1,35 @@
 // Name = Shaurya Malhotra
 // Roll No. = 2010990918
-// Set No. = 
+// Set No. = 3
 // Q1
 
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
 
+int main(){
+    int n; cin>>n;
+    vector<int> arr(n);
+
+   
+    for(auto &i: arr) cin>>i;
+
+     
+    sort(arr.begin(), arr.end());
+
+     
+    int i = 0,  j = n/2 + n%2;
+
+     
+    vector<int> ans;
+
+    
+    while(i < n/2 + n%2 || j < n){
+        if(i < n/2 + n%2) ans.push_back(arr[i++]);
+        if(j < n) ans.push_back(arr[j++]);
+    }
+
+     
+    for(auto &i: ans) cout<<i<<" ";
+    cout<<endl;
+}
