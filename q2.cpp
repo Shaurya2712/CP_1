e5c0095a052977f1612d7b79f24373914ff7a4ea
samridhi14 <83155658+Shaurya2712@users.noreply.github.com>
2022-08-26 15:29:26 +0530

    q 2 submitted

--- q2.cpp
+++ q2.cpp
@@ -1,4 +1,41 @@
 // Name = Shaurya Malhotra
 // Roll No. = 2010990918
-// Set No. = 
+// Set No. = 3
 // Q2
+
+ 
+ 
+#include <bits/stdc++.h>
+
+using namespace std;
+  
+ 
+int countPair(int arr[], int n, int sum)
+{
+    int count = 0;
+  
+    // for loop for checking sum
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++)
+            if (arr[i] + arr[j] == sum)
+                count++;
+  
+    return count;
+}
+  
+ 
+int main()
+{
+     // sorry !
+    cout<<"please input the array and sum by yourself in code "<<endl;
+
+    int arr[] = { 1, 5, 7, -1, 5 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    
+    int sum = 10;
+     
+cout << "number of pairs in the array = "
+         << countPair(arr, n, sum);
+    return 0;
+}
